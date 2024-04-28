7b8c7c0356b0b91f67b73ba8facd8d6a505c93e8
Sanups-777 <u2104063@rajagiri.edu.in>
2024-04-28 19:34:31 +0530

    search bar

--- local_modules/admin_modules/admin.js
+++ local_modules/admin_modules/admin.js
@@ -2,22 +2,13 @@ const express = require('express');
 const { ObjectId } = require('mongodb');
 const { default: mongoose } = require('mongoose');
 const router = express.Router();
+const Usersdata = require('../model');
 
-let db;
+let mongooseConnection; // Change db to mongooseConnection
 function init(dbConnection) {
-  db = dbConnection;
+  mongooseConnection = dbConnection;
   // console.log("connected succesfully")
 }
-
-const udetailSchema = new mongoose.Schema({
-  id:ObjectId,
-  name: String,
-  username: String,
-  email: String,
-  phno: String
-});
-
-const Usersdata = mongoose.model('users', udetailSchema);
 
 router.get('/details', (req, res) => {
   Usersdata.find({})
@@ -37,13 +28,13 @@ router.post("/remuser", async (req, res) => {
     var name = req.body.nrem;
     var result;
     try {
-    result = await db.collection("users").findOne({ username: name });
+    result = await Usersdata.findOne({ username: name }); // Use Usersdata model
     } catch (err) {
     console.log("User does not exist");
     }
     console.log(result);
 
-    db.collection("users").deleteOne({ username: name });
+    await Usersdata.deleteOne({ username: name }); // Use Usersdata model and await the deletion
     console.log("user deleted");
     let a = "Admin";
     res.render("adminp", {
--- local_modules/user_modules/user.js
+++ local_modules/user_modules/user.js
@@ -1,37 +1,54 @@
-let mongooseConnection;
-function init(db) {
-  mongooseConnection = db;
-}
-
 const express = require('express');
+const { ObjectId } = require('mongodb');
+const mongoose = require('mongoose');
 const router = express.Router();
+const Usersdata = require('../model');
 
+let db; // Change db to mongooseConnection
+function init(dbConnection) {
+  db = dbConnection;
+  // console.log("connected succesfully")
+}
 router.post("/reset_pass", async (req, res) => {
-  
-    var email = req.body.email_reset;
-    var original_password = req.body.original_password_reset;
-    var new_password = req.body.new_password_reset;
-    try {
-        var result = await db.collection("users").findOne({ email: email });
-      } catch (err) {
-        console.log("User does not exist");
-      }
-    console.log(result);
-  
-    if (result) {
-      if (result.password === original_password) {
-        db.collection("users").updateOne({ email: email },
-            { $set: { password: new_password } });
-  
-        return res.redirect("login.html");
-      } else {
-        console.log("INVALID USERNAME OR PASSWORD");
-      }
-    } else {
-      console.log("INVALID USERNAME OR PASSWORD");
+  const { email_reset, original_password_reset, new_password_reset } = req.body;
+
+  try {
+    const user = await Usersdata.findOne({ email: email_reset });
+
+    if (!user) {
+      console.log("User does not exist");
+      return res.status(404).send("User does not exist");
     }
+
+    if (user.password !== original_password_reset) {
+      console.log("Invalid username or password");
+      return res.status(400).send("Invalid username or password");
+    }
+
+    await Usersdata.updateOne({ email: email_reset }, { $set: { password: new_password_reset } });
+
+    // Redirect to the login page
+    return res.redirect("/login.html");
+  } catch (err) {
+    console.error("Error resetting password:", err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 
+router.get("/search", async (req, res) => {
+  try {
+    const searchQuery = req.query.name;
+    if (!searchQuery || searchQuery.trim() === "") {
+      return res.status(400).json({ error: true, message: "Name query parameter is required" });
+    }
+
+    const users = await Usersdata.find({ name: { $regex: searchQuery, $options: 'i' } });
+    res.render('homepage', { userlist: users });
+  } catch (error) {
+    console.error("Error searching users:", error);
+    res.status(500).json({ error: true, message: "Internal server error" });
+  }
+});
 
 module.exports = { init, router };
