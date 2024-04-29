437f23086e1c151a0500c809346b9b4d447151a0
Sanups-777 <u2104063@rajagiri.edu.in>
2024-04-29 12:57:10 +0530

    models

--- local_modules/admin_modules/admin.js
+++ local_modules/admin_modules/admin.js
@@ -2,8 +2,7 @@ const express = require('express');
 const { ObjectId } = require('mongodb');
 const { default: mongoose } = require('mongoose');
 const router = express.Router();
-const { Usersdata, Buisnessdata } = require('./models');
-const buisnessdata=require('../model');
+const { Usersdata, Buisnessdata } = require('../model');
 let mongooseConnection; // Change db to mongooseConnection
 function init(dbConnection) {
   mongooseConnection = dbConnection;
@@ -38,6 +37,39 @@ router.post("/uremove", async (req, res) => {
       await Usersdata.deleteOne({ email: email });
       console.log("User deleted");
       res.redirect("/admin/details");
+  }
+
+    
+});
+
+router.get('/bdetails', (req, res) => {
+  Buisnessdata.find({})
+    .then((data) => {
+      res.render('details', {
+        userlist: data
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+router.post("/uremove", async (req, res) => {
+  
+    var email = req.body.nrem;
+    var result;
+    try {
+    result = await Buisnessdata.findOne({ email: email }); // Use Usersdata model
+    } catch (err) {
+    console.log("User does not exist");
+    }
+    if (!result) {
+      console.log("User not found");
+  } else {
+      await Buisnessdata.deleteOne({ email: email });
+      console.log("User deleted");
+      res.redirect("/admin/bdetails");
   }
 
     
