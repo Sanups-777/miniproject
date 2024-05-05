a363421ac2c6b54721e99c00637cb1e798de97d1
Jaseerraja <u2104030@rajagiri.edu.in>
2024-05-05 22:39:45 +0530

    changed homepage and added routess to viewbusiness

--- local_modules/routes/routes.js
+++ local_modules/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
-const { Usersdata, Buisnessdata } = require('../models/model.js');
+const path = require("path");
+const { Usersdata, Buisnessdata } = require("../models/model.js");
 
 let db;
 function init(dbConnection) {
@@ -10,31 +10,32 @@ function init(dbConnection) {
 }
 
 router.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/login.html"));
+  res.sendFile(path.join(__dirname, "../../public/login.html"));
 });
 
 router.get("/signup", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/reg.html"));
+  res.sendFile(path.join(__dirname, "../../public/reg.html"));
 });
 
 router.get("/mainpage", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/mainpage.html"));
+  res.sendFile(path.join(__dirname, "../../public/mainpage.html"));
 });
 router.get("/homepage", async (req, res) => {
-    const page = req.query.page || 1;
-    const limit = 20;
-    const skip = (page - 1) * limit;
-  
-    try {
-      const data = await Buisnessdata.find({})
-        .skip(skip)
-        .limit(limit);
-  
-      res.render("homepage", { blist: data });
-    } catch (err) {
-      console.error('Error fetching Buisnessdata collection:', err);
-      res.status(500).send("Internal Server Error");
-    }
+  const page = req.query.page || 1;
+  const limit = 20;
+  const skip = (page - 1) * limit;
+
+  try {
+    const data = await Buisnessdata.find({}).skip(skip).limit(limit);
+
+    res.render("homepage", { blist: data });
+  } catch (err) {
+    console.error("Error fetching Buisnessdata collection:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+router.get("/homepage/viewbusiness", (req, res) => {
+  res.render("viewbuisness");
 });
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxFIX RESET XXXXXXXXXXXXXXX//////////////////
 router.get("/reset", (req, res) => {
@@ -48,17 +49,14 @@ router.get("/reset", (req, res) => {
   //     console.error(err);
   //     res.status(500).send("Internal Server Error");
   //   }));)
-    res.sendFile(path.join(__dirname, "../../public/reset.html"));
+  res.sendFile(path.join(__dirname, "../../public/reset.html"));
 });
 
 router.get("/mainpage", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/mainpage.html"));
+  res.sendFile(path.join(__dirname, "../../public/mainpage.html"));
 });
 router.get("/mainpage", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/mainpage.html"));
+  res.sendFile(path.join(__dirname, "../../public/mainpage.html"));
 });
-
-
-
 
 module.exports = { init, router };
