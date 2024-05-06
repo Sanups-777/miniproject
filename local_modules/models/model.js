153d41822108c717411b5a125afd29ee21b0013f
Jaseerraja <u2104030@rajagiri.edu.in>
2024-05-06 23:12:05 +0530

    fixed business login and login page

--- local_modules/models/model.js
+++ local_modules/models/model.js
@@ -4,13 +4,9 @@ const udetailSchema = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
   name: String,
   username: String,
-  email: {type:String,
-    required:true,
-    unique:true,
-    lowercase:true},
+  email: { type: String, required: true, unique: true, lowercase: true },
   phno: String,
-  password: {type:String,
-    required:true},
+  password: { type: String, required: true },
 });
 
 const Usersdata = mongoose.model("users", udetailSchema);
@@ -18,15 +14,11 @@ const Usersdata = mongoose.model("users", udetailSchema);
 const bdetailSchema = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
   name: String,
-  email: {type:String,
-    required:true,
-    unique:true,
-    lowercase:true},
+  email: { type: String, required: true, unique: true, lowercase: true },
   phone: String,
-  password: {type:String,
-    required:true},
+  password: { type: String, required: true },
   services: [String],
-  
+  description: String,
 });
 
 const Buisnessdata = mongoose.model("business-ls", bdetailSchema);
