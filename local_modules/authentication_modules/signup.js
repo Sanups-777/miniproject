337a9e5feb404f3b1794848b8b65530d631eb606
janicefrancis27 <u2104029@rajagiri.edu.in>
2024-05-12 06:17:16 -0700

    c2

--- local_modules/authentication_modules/signup.js
+++ local_modules/authentication_modules/signup.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { Usersdata  } = require("../models/model");
-
+const { Buisnessdata } = require("../models/model");
 const handleErrors = (err) =>{
   console.log(err.message, err.code);
   let error = {
-     email : '' ,password: ''
-  };
+   name : '',username :'', email : '' ,phno : '',password: '' , conpassword : ''
+ };
   //validation errors
   if(err.message.includes('users validation failed'))
     {
@@ -17,43 +17,43 @@ const handleErrors = (err) =>{
     return error;
     
 }
-// router.post("/business", async (req, res) => {
-//   var name = req.body.name;
-//   var username = req.body.username;
-//   var email = req.body.email;
-//   var phno = req.body.phno;
-//   var password = req.body.password;
+ router.post("/business", async (req, res) => {
+  var name = req.body.name;
+   var username = req.body.username;
+   var email = req.body.email;
+   var phno = req.body.phno;
+   var password = req.body.password;
 
-//   try {
-//     const newData = await Buisnessdata.create({
-//       name: name,
-//       username: username,
-//       email: email,
-//       phno: phno,
-//       password: password,
-//     });
+   try {
+     const newData = await Buisnessdata.create({
+       name: name,
+       username: username,
+       email: email,
+       phno: phno,
+       password: password,
+     });
 
-//     console.log("Record Inserted Successfully:", newData._id);
-//     res.redirect("/homesaver/login");
-//   } catch (err) {
-//     console.error("Error inserting record:", err);
-//     return res.status(500).json({ error: "Internal server error" });
-//   }
-// });
+     console.log("Record Inserted Successfully:", newData._id);
+     res.redirect("/homesaver/login");
+   } catch (err) {
+     console.error("Error inserting record:", err);
+     return res.status(500).json({ error: "Internal server error" });
+   }
+ });
 
 router.post("/users", async (req, res) => {
-  //var name = req.body.name;
-  //var username = req.body.username;
+  var name = req.body.name;
+  var username = req.body.username;
   var email = req.body.email;                                     //sigup_post
-  //var phno = req.body.phno;
+  var phno = req.body.phno;
   var password = req.body.password;
 
   try {
     const newData = await Usersdata.create({
-     // name: name,
-      //username: username,
+      name: name,
+      username: username,
       email: email,
-      //phone : phno,
+      phone : phno,
       password: password,
     });
 
--- local_modules/models/model.js
+++ local_modules/models/model.js
@@ -4,6 +4,23 @@ const { isEmail } = require('validator');
 const udetailSchema = new mongoose.Schema({
   //id: mongoose.Schema.Types.ObjectId,
   //name: String,
+  name: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[a-zA-Z]+$/.test(v); // Only alphabets allowed
+      },
+      message: props => `${props.value} contains numbers!`
+    }
+  },
+
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
   //username: String,
   email: {
     type:String,
@@ -12,25 +29,93 @@ const udetailSchema = new mongoose.Schema({
     lowercase: true,
     validate: [isEmail, 'Please enter a valid email']
   },
+
+  phno: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{10}$/.test(v); // 10 digits only
+      },
+      message: props => `${props.value} is not a valid phone number!`
+      }
+    },
   //phno: String,
   password:{
     type:String,
     required: true,
     minlength: [8,'Minimum password length is 8 charecters']
-  } 
+  } ,
+  conpassword: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return v === this.password; // Must match password field
+      },
+      message: props => `Passwords do not match!`
+    }
+  }
 });
 
 const Usersdata = mongoose.model("users", udetailSchema);
 
-// const bdetailSchema = new mongoose.Schema({
+const bdetailSchema = new mongoose.Schema({
 //   id: mongoose.Schema.Types.ObjectId,
 //   name: String,
-//   email: String,
+name: {
+  type: String,
+  required: true,
+  validate: {
+    validator: function(v) {
+      return /^[a-zA-Z]+$/.test(v); // Only alphabets allowed
+    },
+    message: props => `${props.value} contains numbers!`
+  }
+},
+username: {
+  type: String,
+  required: true,
+  unique: true
+},
+   email: {
+    type:String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
 //   phone: String,
+phno: {
+  type: String,
+  required: true,
+  validate: {
+    validator: function(v) {
+      return /^\d{10}$/.test(v); // 10 digits only
+    },
+    message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+password:{
+  type:String,
+  required: true,
+  minlength: [8,'Minimum password length is 8 charecters']
+} ,
+confirmPassword: {
+  type: String,
+  required: true,
+  validate: {
+    validator: function(v) {
+      return v === this.password; // Must match password field
+    },
+    message: props => `Passwords do not match!`
+  }
+}
 //   services: [String],
-// });
+ });
 
-//const Buisnessdata = mongoose.model("business-ls", bdetailSchema);
+const Buisnessdata = mongoose.model("business-ls", bdetailSchema);
 
 module.exports = { Usersdata};
 // module.exports=Usersdata;
+module.exports = { Buisnessdata};
