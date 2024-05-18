4056178c6817fc75877ba2f8e65cab7853760f60
Jaseerraja <u2104030@rajagiri.edu.in>
2024-05-18 12:08:12 +0530

    added description

--- local_modules/business_modules/business.js
+++ local_modules/business_modules/business.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 router.get("/services", async (req, res) => {
   const bId = req.query.bId;
-  res.render("business/services", { bId: bId });
+  const business = await Buisnessdata.findById(bId).exec();
+  if (!business) {
+    return res.status(404).send("Business not found");
+  }
+  res.render("business/services", { business: business, bId: bId });
 });
 
 router.post("/acceptservice", async (req, res) => {
@@ -12,23 +16,33 @@ router.post("/acceptservice", async (req, res) => {
   const service = req.body.service;
   const min = req.body.minprice;
   const max = req.body.maxprice;
+  const desc = req.body.description;
 
   if (!bId) {
     return res.status(400).send("Business ID is missing");
   }
+
   try {
     const business = await Buisnessdata.findById(bId).exec();
     if (!business) {
       return res.status(404).send("Business not found");
     }
 
-    // Create a new service object and add it to the services array
+    // Create a new service object
     const newService = {
       name: service,
       minprice: min,
       maxprice: max,
     };
-    business.services.push(newService); // This line adds the new service to the existing array
+    console.log(business.description);
+    // Only add the description if it is 'null' or empty in the business object
+    if (!business.description) {
+      if (desc !== "null" && desc.trim() !== "") {
+        business.description = desc;
+      }
+    }
+
+    business.services.push(newService); // Adds the new service to the existing array
 
     await business.save();
     console.log("Added service successfully");
